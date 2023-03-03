385e9b5adffb251f8f1523bf97a597162893c8c7
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-03-04 00:12:56 +0530

    Update totalcharacter.java
    Java Program to count the total number of characters in a string

--- String programs/totalcharacter.java
+++ String programs/totalcharacter.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String s="hero has fallen";
+        int i=0,count=0;
+        while(i<=s.length()){
+        if(s.charAt(i)!=' ')
+        {
+          count++;
+        }
+        i++;
+        }
+        System.out.println(count);
+    }
+}
 
