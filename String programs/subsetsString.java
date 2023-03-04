9592f6bd316bc892911cb03161466f872055b0b8
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-03-04 11:32:21 +0530

    Update subsetsString.java
    Java Program to find all subsets of a string

--- String programs/subsetsString.java
+++ String programs/subsetsString.java
@@ -1,1 +1,26 @@
+package practice;
+import java.util.Arrays; 
+public class  subsetsString{
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String s1="bob";
+       int l=s1.length();
+       int  temp=0;
+       
+        String[] st=new String[l*(l+1)/2];
+        
+		   for(int i=0;i<l;i++) {
+			   for(int j=i;j<l;j++) {
+				   st[temp]=s1.substring(i,j+1);
+			        temp++;		
+			   }
+		   }
+		   
+	   
+        System.out.println(" substrings of string are ");  
+        for(int i = 0; i < st.length; i++) {  
+            System.out.println(st[i]);  
+            }           		      
+	}
+}
