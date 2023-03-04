0ffdce6c470076bc0a644b0cb7a6074cfcfbeaf6
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-03-04 12:32:42 +0530

    Update reverseString.java
    reserve a string in Java

--- String programs/reverseString.java
+++ String programs/reverseString.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;  
+public class reverseString 
+{  
+public static void main(String[] arg)  
+{  
+reverseString rev=new reverseString();  
+Scanner sc=new Scanner(System.in);  
+System.out.print("Enter a string : ");  
+String  s=sc.nextLine();      
+System.out.println("Reverse String  is : "+rev.reverse(s));   
+}     
+String reverse(String str)  
+{  
+String rev="";  
+for(int i=str.length();i>0;--i)  
+{  
+rev=rev+(str.charAt(i-1));   
+}  
+return rev;  
+}  
+}  
