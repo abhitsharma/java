75df376f0cb57013c287eddd4b591bf65ad46c67
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-03-07 00:54:43 +0530

    Update sort.java
    program to Sort an Array in Java Using the sort() Method

--- Array/sort.java
+++ Array/sort.java
@@ -1,1 +1,22 @@
+package practice;
+import java.util.Arrays;
+import java.util.Scanner;
+public class sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     int arr[]=new int[n];
+     for(int i=0;i<n;i++) {
+    arr[i]=s.nextInt();
+     }
+     Arrays.sort(arr);
+     for(int i=0;i<n;i++) {
+    	 System.out.println(arr[i]); 
+    	     }
+    	     
+	}
+   
+}
 
