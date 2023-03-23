ac216cb0808d37c1364c5731f39a4d3e53951b0e
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-03-23 21:26:50 +0530

    Create DeleteStart.java

---
+++ Circular Linked List/DeleteStart.java
@@ -0,0 +1,69 @@
+public class DeleteStart {    
+    public class Node{  
+        int data;  
+        Node next;  
+        public Node(int data) {  
+            this.data = data;  
+        }  
+    }    
+    public Node head = null;  
+    public Node tail = null;  
+    public void add(int data){   
+        Node newNode = new Node(data);  
+        if(head == null) {  
+            head = newNode;  
+            tail = newNode;  
+            newNode.next = head;  
+        }  
+        else {  
+            tail.next = newNode; 
+            tail = newNode;  
+            tail.next = head;  
+        }  
+    }  
+    public void deleteStart() { 
+        if(head == null) {  
+            return;  
+        }  
+        else { 
+            if(head != tail ) {  
+                head = head.next;  
+                tail.next = head;  
+            } 
+            else {  
+                head = tail = null;  
+            }  
+        }  
+    }  
+  
+    public void display() {  
+        Node current = head;  
+        if(head == null) {  
+            System.out.println("List is empty");  
+        }  
+        else {  
+             do{    
+                System.out.print(" "+ current.data);  
+                current = current.next;  
+            }while(current != head);  
+            System.out.println();  
+        }  
+    }  
+  
+    public static void main(String[] args) {  
+        DeleteStart cl = new DeleteStart();  
+        //Adds data to the list  
+        cl.add(1);  
+        cl.add(2);  
+        cl.add(3);  
+        cl.add(4);  
+        //Printing original list  
+        System.out.println("Original List: ");  
+        cl.display();  
+        while(cl.head != null) {  
+            cl.deleteStart();  
+            System.out.println("Updated List: ");  
+            cl.display();  
+        }  
+    }  
+}  
